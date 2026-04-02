d01bbf18e969042e1f796a6b05050244e39a0a7d
moonlightOS-Meow <karlik8820@gmail.com>
2026-04-02 21:18:08 +0200

    COPILOT GOT IT WRONG

--- site-src/vite.config.js
+++ site-src/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    svgr() // Import SVGs as React components (favicons, icons, etc.)
   ],
   base: '/moonlightos-meow/',
 })
